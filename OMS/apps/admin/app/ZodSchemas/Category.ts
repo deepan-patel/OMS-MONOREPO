8ecc3fac82a2b66d9cda8b46fa4a4bf8eb2214c6
Deepan Patel <55546591+deepan-patel@users.noreply.github.com>
2026-04-07 20:13:17 -0400

    Revert "added some repos"
    This reverts commit 7a6e4dd2d72ed9b00ae29912ad42a639a0b0bdc8.

--- OMS/apps/admin/app/ZodSchemas/Category.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod"
-
-export const CategorySchema = z.object({
-    name: z.string().min(1, "Name must be at least 1"),
-
-});
-
-export type CategoryFormInputs = z.infer<typeof CategorySchema>;
--- OMS/apps/admin/app/ZodSchemas/Order.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod"
-
-export const OrderSchema = z.object({
-    amount: z.number().min(1, "Amount must be at least 1"),
-    userId: z.string().min(1, "User ID must be at least 1"),
-    status: z.enum(["pending", "processing", "success", "failed"]),
-});
-
-export type OrderFormInputs = z.infer<typeof OrderSchema>;
