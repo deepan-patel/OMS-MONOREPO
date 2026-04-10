1848a9774d5c51f6f39a5d41d9e3045f30f834ee
Deepan Patel <55546591+deepan-patel@users.noreply.github.com>
2026-04-10 06:18:35 -0400

    added clerk auth
    -- added clerk sign in and sign up page

--- OMS/apps/client/src/app/layout.tsx
+++ OMS/apps/client/src/app/layout.tsx
@@ -7,6 +7,9 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner"
 import { ThemeProvider } from "@/components/theme/theme-provider"
 
+// clerk imports
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
@@ -30,24 +33,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl p-4">
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <NavBar />
-            {children}
-            <Toaster />
-          </ThemeProvider>
-          <Footer />
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div className="mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl p-4">
+
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <NavBar />
+              {children}
+              <Toaster />
+            </ThemeProvider>
+
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- OMS/apps/client/src/components/web/Navbar.tsx
+++ OMS/apps/client/src/components/web/Navbar.tsx
@@ -4,6 +4,7 @@ import { buttonVariants } from "../ui/button";
 import SearchBar from "./SearchBar";
 import ShoppingCartIcon from "./ShoppingCartIcon";
 import { ThemeToggle } from "../theme/ThemeToggle";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 
 export default function NavBar() {
     return (
@@ -20,8 +21,27 @@ export default function NavBar() {
                 <Bell className="hover:text-primary w-9 h-9 text-gray-500" />
                 <ThemeToggle />
                 <ShoppingCartIcon />
-                <Link className={buttonVariants({ variant: "default", className: "hover:text-primary" })} href="/sign-in" >Sign In</Link>
+                <Show when="signed-out">
+                    <Link className={buttonVariants({ variant: "default", className: "hover:text-primary" })} href="/sign-in" >Sign In</Link>
+                </Show>
+                <Show when="signed-in">
+                    <UserButton />
+                </Show>
             </div>
+
+            {/* <div>
+                <Show when="signed-out">
+                    <SignInButton />
+                    <SignUpButton>
+                        <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                            Sign Up
+                        </button>
+                    </SignUpButton>
+                </Show>
+                <Show when="signed-in">
+                    <UserButton />
+                </Show>
+            </div> */}
 
         </nav>
     )
